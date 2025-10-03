d73efd9d2b8e54731d7d5c8bb8663958f08c54b9
Malong Guo <linuxc2012@163.com>
2025-10-03 14:24:33 +0800

    1. 为ResultErr结构体添加接口实现    - 新增ResultErrI接口定义    - 实现GetCode()和GetMsg()方法    - 实现error接口的Error()方法    - 添加接口实现验证

--- httpm/httpm.go
+++ httpm/httpm.go
@@ -6,10 +6,30 @@ type ResultOk struct {
 	Data any    `json:"data,omitempty"`
 }
 
+type ResultErrI interface {
+	GetCode() int32
+	GetMsg() string
+	error
+}
+
 type ResultErr struct {
 	Code int32  `json:"code" default:"1"`
 	Msg  string `json:"msg" default:"error"`
 }
+
+func (r ResultErr) GetCode() int32 {
+	return r.Code
+}
+
+func (r ResultErr) GetMsg() string {
+	return r.Msg
+}
+
+func (r ResultErr) Error() string {
+	return r.Msg
+}
+
+var _ ResultErrI = (*ResultErr)(nil)
 
 func NewResultOk(data any, msg ...string) *ResultOk {
 	msgStr := "success"
